4211c2faa46777633ca5927c55061032bff44486
Hafeez Syed <hafeez.syed@team.telstra.com>
2017-02-24 16:57:00 +1100

    initial project structuring

---
+++ api/test/integration-test.js
@@ -0,0 +1,16 @@
+/**
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ 
+ Author: Hafeez Syed
+ File: integration-test.js
+ Created on 24/02/2017 12:42 PM
+ Project: Node-Docker-Microservices
+ Description: < Description here >
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
+'use strict';
---
+++ m-services/booking-service/src/models/collection1.js
@@ -0,0 +1,16 @@
+/**
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ 
+ Author: Hafeez Syed
+ File: collection1.js
+ Created on 24/02/2017 1:58 PM
+ Project: Node-Docker-Microservices
+ Description: < Description here >
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
+'use strict';
